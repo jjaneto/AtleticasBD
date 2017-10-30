564d295fbc7528c5e5fa92bd53f95d0dec52d8ab
jjaneto <jjaneto@dcomp.ufs.br>
2017-10-30 00:41:00 -0300

    Adicionando Classes GUI de Adicao, Exclusao e Edicao

---
+++ src/gui/FrameAdicionar.java
@@ -0,0 +1,146 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.HeadlessException;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import javafx.stage.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import net.miginfocom.swing.MigLayout;
+import utils.BancoControle;
+import utils.Membro;
+
+/**
+ *
+ * @author jjaneto
+ */
+public class FrameAdicionar extends JFrame {
+
+    private JPanel panelPrincipal;
+    private JPanel panelBotoes;
+    private JTextField jtfMatAtletica;
+    private JTextField jtfMatUniversidade;
+    private JTextField jtfNome;
+    private JTextField jtfCPF;
+    private JTextField jtfRG;
+    private JTextField jtfOcupacao;
+    private JTextField jtfStatus;
+    private JButton btOk;
+    private JButton btCancelar;
+
+    public FrameAdicionar() throws HeadlessException {
+        setLayout(new BorderLayout());
+        constroiEAdicionaPanelBotoes();
+        constroiEAdicionaPanelPrincipal();
+
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        //pack();
+        setMinimumSize(new Dimension(400, 280));
+        setLocationRelativeTo(null);
+        setTitle("Adicionar Membro");
+        setVisible(true);
+    }
+
+    public void constroiEAdicionaPanelBotoes() {
+        panelBotoes = new JPanel(new GridLayout(1, 2, 1, 1));
+        this.add(panelBotoes, BorderLayout.SOUTH);
+
+        btOk = new JButton("Adicionar");
+        btOk.setIcon(new ImageIcon("./img/ok.png"));
+        btOk.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Membro mbr = new Membro();
+                mbr.setMatricula_atletica(jtfMatAtletica.getText());
+                mbr.setMatricula_universidade(jtfMatUniversidade.getText());
+                mbr.setNome(jtfNome.getText());
+                mbr.setCPF(jtfCPF.getText());
+                mbr.setRG(jtfRG.getText());
+                mbr.setMembro_desde(LocalDate.now());
+                mbr.setOcupacao(jtfOcupacao.getText());
+                mbr.setStatus(jtfStatus.getText());
+                if(BancoControle.adicionaMembro(mbr)){
+                    JOptionPane.showMessageDialog(getContentPane(), 
+                            "Adição concluída com sucesso!", 
+                            "Êxito", JOptionPane.INFORMATION_MESSAGE);
+                    dispose();
+                }else{
+                    
+                }
+            }
+        });
+
+        btCancelar = new JButton("Cancelar");
+        btCancelar.setIcon(new ImageIcon("./img/cancel.png"));
+        btCancelar.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+            }
+        });
+
+        panelBotoes.add(btCancelar);
+        panelBotoes.add(btOk);
+    }
+
+    public void constroiEAdicionaPanelPrincipal() {
+        panelPrincipal = new JPanel(new MigLayout("debug, fillx"));
+        this.add(panelPrincipal, BorderLayout.CENTER);
+        
+        System.out.println(BancoControle.getMaxMatriculaAtletica());
+        
+        int novaMatricula = Integer.valueOf(BancoControle.getMaxMatriculaAtletica());
+        
+        JLabel lbMatAtletica = new JLabel("Matrícula da Atlética: ");
+        jtfMatAtletica = new JTextField();
+        jtfMatAtletica.setText(String.valueOf(novaMatricula + 1));
+        jtfMatAtletica.setEditable(false);
+        jtfMatAtletica.setToolTipText("Matrícula escolhida automaticamente. "
+                + "Para editar manualmente, consulte o administrador.");
+        panelPrincipal.add(lbMatAtletica, "split 2");
+        panelPrincipal.add(jtfMatAtletica, "growx, wrap");
+        
+        JLabel lbMatUniversidade = new JLabel("Matrícula da Universidade: ");
+        jtfMatUniversidade = new JTextField();
+        panelPrincipal.add(lbMatUniversidade, "split 2");
+        panelPrincipal.add(jtfMatUniversidade, "growx, wrap");
+        
+        JLabel lbNome = new JLabel("Nome: ");
+        jtfNome = new JTextField();
+        panelPrincipal.add(lbNome, "split 2");
+        panelPrincipal.add(jtfNome, "growx, wrap");
+        
+        JLabel lbCPF = new JLabel("CPF: ");
+        jtfCPF = new JTextField();
+        panelPrincipal.add(lbCPF, "split 2");
+        panelPrincipal.add(jtfCPF, "growx, wrap");
+        
+        JLabel lbRG = new JLabel("RG: ");
+        jtfRG = new JTextField();
+        panelPrincipal.add(lbRG, "split 2");
+        panelPrincipal.add(jtfRG, "growx, wrap");
+        
+        JLabel lbOcupacao = new JLabel("Ocupação: ");
+        jtfOcupacao = new JTextField();
+        panelPrincipal.add(lbOcupacao, "split 2");
+        panelPrincipal.add(jtfOcupacao, "growx, wrap");
+        
+        JLabel lbStatus = new JLabel("Status: ");
+        jtfStatus = new JTextField();
+        panelPrincipal.add(lbStatus, "split 2");
+        panelPrincipal.add(jtfStatus, "growx");
+     
+    }
+
+}
--- src/gui/FramePrincipal.java
+++ src/gui/FramePrincipal.java
@@ -1,12 +1,20 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import utils.BancoControle;
+import utils.Membro;
 
 /**
  *
@@ -22,9 +30,13 @@ public class FramePrincipal extends JFrame {
     private JButton btEditar;
 
     public FramePrincipal() {
+        setLayout(new BorderLayout());
         constroiEAdicionaPanelSuperior();
+        constroiEAdicionaPanelInferior();
 
-        pack();
+        setTitle("Banco de dados da Atletica das Engenharias - UFS");
+        setMinimumSize(new Dimension(300, 300));
+//        pack();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setVisible(true);
@@ -32,26 +44,29 @@ public class FramePrincipal extends JFrame {
 
     public void constroiEAdicionaPanelSuperior() {
         btAdicionar = new JButton("Adicionar");
+        btAdicionar.setIcon(new ImageIcon("./img/add.png"));
         btAdicionar.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if (e.getButton() == MouseEvent.BUTTON1) {
-//                    JOptionPane.showConfirmDialog(getContentPane(), "Haha!");
+                if (SwingUtilities.isLeftMouseButton(e)) {
+                    new FrameAdicionar();
                 }
             }
         });
         
         btExcluir = new JButton("Excluir");
+        btExcluir.setIcon(new ImageIcon("./img/trash.png"));
         btExcluir.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 if (e.getButton() == MouseEvent.BUTTON1) {
-
+                    
                 }
             }
         });
         
         btEditar = new JButton("Editar");
+        btEditar.setIcon(new ImageIcon("./img/edit.png"));
         btEditar.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -61,13 +76,19 @@ public class FramePrincipal extends JFrame {
             }
         });
 
-        panelSuperior = new JPanel(new BorderLayout());
+        panelSuperior = new JPanel(new GridLayout(1, 3, 1, 1));
 
-        panelSuperior.add(btAdicionar, BorderLayout.EAST);
-        panelSuperior.add(btExcluir, BorderLayout.CENTER);
-        panelSuperior.add(btEditar, BorderLayout.WEST);
+        panelSuperior.add(btAdicionar);//, BorderLayout.EAST);
+        panelSuperior.add(btExcluir);//, BorderLayout.CENTER);
+        panelSuperior.add(btEditar);//, BorderLayout.WEST);
 
-        this.add(panelSuperior);
+        this.add(panelSuperior, BorderLayout.NORTH);
+    }
+    
+    public void constroiEAdicionaPanelInferior(){
+        panelInferior = new JPanel(new BorderLayout());
+        panelInferior.add(new JLabel("TABELA DOS MEMBROS VAI AQUI"));
+        this.add(panelInferior, BorderLayout.CENTER);
     }
 
 }
